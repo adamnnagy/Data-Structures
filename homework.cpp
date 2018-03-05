83e3d8281bd8374a829e285295b0bcaa9279375a
Adam Nagy <an2208@nyu.edu>
2018-03-05 22:10:29 +0400

    added equal

--- homework.cpp
+++ homework.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "set.h"
 
+//testing if an element is a member of the set
+
 bool Set::Member(int e) {
   for (size_t i = 0; i < size; i++) {
     if (e == set[i]) {
@@ -11,27 +13,45 @@ bool Set::Member(int e) {
   }
 }
 
-void Set::Add(const int e){
-  int i = 0;
-  while (e < set[i]) {
-    i++;
-  }
-  set[i] = e;
-  size++;
+//adding an element and sorting the array
 
+void Set::AddElem(const int e){
+  if (size + 1 == maxCardinality) {
+    std::cout << "you have reached the maximum elements in the Set." << '\n';
+    return;
+  }
+  if(e > set[size - 1]) {
+    set[size] = e;
+    size++;
+  } else {
+    int i = size - 1;
+    size++;
+    while (e < set[i]) {
+      set[i + 1] = set[i];
+      if (i < 1) {
+        set[0] = e;
+        break;
+      }
+      i--;
+    }
+    set[i] = e;
+  }
 }
 
+//returns the index of an element if it's the member of the set
+
 int Set::ElemIndex(const int e) {
-  if (Member(e)) {
     for (size_t i = 0; i < size; i++) {
       if (e == set[i]) {
         return i;
       }
     }
-  } else {
     std::cout << "Element is not in the set." << '\n';
-  }
+    return 0;
 }
+
+
+//removes an element from the set
 
 void Set::RmvElem(const int e) {
   for (size_t i = ElemIndex(e); i < size; i++) {
@@ -40,13 +60,17 @@ void Set::RmvElem(const int e) {
   size--;
 }
 
+//prints the set
+
 void Set::Print() {
   std::cout << "{ ";
-  for (size_t i = 0; i < size; i++) {
+  for (size_t i = 0; i < size - 1; i++) {
     std::cout << set[i] << ", ";
   }
-  std::cout << " }" << '\n';
+  std::cout << set[size-1] << " }" << '\n';
 }
+
+//copies the set
 
 void Set::Copy(Set& copyFrom) { //copy a to b
   size = copyFrom.Card();
@@ -56,42 +80,81 @@ void Set::Copy(Set& copyFrom) { //copy a to b
   }
 }
 
-bool Set::Equal(Set& a, Set& b) {
-
+bool Set::Equal(Set& a) {
+  if (a.Card() != this->Card()) {
+    return false;
+  } else {
+    for (size_t i = 0; i < size; i++) {
+      if (*(set+i) != *(a.Elems()+i)) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
 
-int* Set::Intersect(Set& a, Set& b) {
-
+int* Set::Intersect(Set& a) {
+  int *intersection;
+  intersection = new int[(a.Card() > size) ? size : a.Card()];
+  for (size_t i = 0; i < size; i++) {
+    if (*(a.Elems()+i) == set[i]) {
+      intersection[i] = set[i];
+    }
+  }
 }
 
-int* Set::Union(Set& a, Set& b) {
-
+int* Set::Union(Set& a) {
+  int *uni;
+  uni = new int[(a.Card() > size) ? a.Card() : size];
 }
 
 
 
 
 int main() {
+
+  //init set
   Set asdf;
+  //add elements to asdf
   asdf.AddElem(3);
   asdf.AddElem(1);
   asdf.AddElem(4);
+
+  //test Member() function
+  asdf.Member(3);
+
+  //init set fdsa
   Set fdsa;
+  //add elements
   fdsa.AddElem(9);
   fdsa.AddElem(8);
   fdsa.AddElem(7);
+  fdsa.AddElem(20);
+  fdsa.AddElem(0);
+
+  //print the two sets
   asdf.Print();
   fdsa.Print();
 
-//test copyFrom
+  //test Equal() function
+  std::cout << fdsa.Equal(asdf) << '\n';
 
-  std::cout << asdf.Elems() << '\n';
-
+  //test Copy() function
   asdf.Copy(fdsa);
+  asdf.Print();
+  std::cout << fdsa.Equal(asdf) << '\n';
 
+  //test ElemIndex() function
+  std::cout << asdf.ElemIndex(20);
+  std::cout << asdf.ElemIndex(12);
+
+  //test RmvElem() function
+  asdf.RmvElem(20);
   asdf.Print();
 
 
-  std::cout << "maxcard " << asdf.MaxCard() << '\n';
-  std::cout << "card " << asdf.Card() << '\n';
+  std::cout << "intersection:" << '\n';
+  for (size_t i = 0; i < asdf.Card(); i++) {
+    std::cout << asdf.Intersect(fdsa)[i] << '\n';
+  }
 }
--- set.h
+++ set.h
@@ -3,20 +3,20 @@ private:
   int maxCardinality;
   int size;
   int *set;
-  int ElemIndex(int e);
 public:
+  int ElemIndex(int e);
   int MaxCard() {return maxCardinality;}; // The maximal cardinality of the set
   int* Elems() {return set;}; // An integer array containing all the elements of the set
-  int Card() const {return size;}; // The effective number of element in the set
+  int Card() const {return size;}; // The effective number of elements in the set
   Set(){maxCardinality = 200; set = new int[maxCardinality];}; //default constructor
   Set(const int num) {maxCardinality = num; set = new int[maxCardinality];}; //One constructor with one argument: the maximal cardinality of the set
   void AddElem(const int e); // Add a new element to the set
   void RmvElem(int e); //  remove one element from the list (by shifting the other elements)
   bool Member(int e); // Verify if a given element is member of the set
   void Copy(Set& copyFrom); // copy one set in another
-  bool Equal(Set& a, Set& b); //verify the equality of two sets
-  int* Intersect(Set& a,Set& b); //compute the intersection of two sets
-  int* Union(Set& a,Set& b); //compute the union of two sets
+  bool Equal(Set& a); //verify the equality of two sets
+  int* Intersect(Set& a); //compute the intersection of two sets
+  int* Union(Set& a); //compute the union of two sets
   void Print(); //display the set, use the mathematics notation
 
   virtual ~Set () { delete [] set;}; //One destructor
