4965006032d45359662d40afbccedfdb5f6323c6
Adam Nagy <an2208@nyu.edu>
2018-03-05 23:56:41 +0400

    submission

--- homework.cpp
+++ homework.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 #include "set.h"
 
-//testing if an element is a member of the set
+//Copy Constructor
+Set::Set(const Set &obj) {
+   std::cout << "Copy constructor allocating ptr." << '\n';
+   set = new int;
+   *set = *obj.set;
+   int size = obj.size; // copy the value
+}
 
+
+//testing if an element is a member of the set
 bool Set::Member(int e) {
   for (size_t i = 0; i < size; i++) {
     if (e == set[i]) {
@@ -46,7 +54,6 @@ int Set::ElemIndex(const int e) {
         return i;
       }
     }
-    std::cout << "Element is not in the set." << '\n';
     return 0;
 }
 
@@ -94,27 +101,44 @@ bool Set::Equal(Set& a) {
 }
 
 int* Set::Intersect(Set& a) {
-  int *intersection;
-  intersection = new int[(a.Card() > size) ? size : a.Card()];
+  Set intersection;
+  intersection.Copy(a);
+  intersection.Print();
   for (size_t i = 0; i < size; i++) {
     if (*(a.Elems()+i) == set[i]) {
       intersection[i] = set[i];
     }
   }
+  return intersection;
 }
 
 int* Set::Union(Set& a) {
-  int *uni;
-  uni = new int[(a.Card() > size) ? a.Card() : size];
+  //create union as set
+  Set uni;
+  //adding one set
+  for (size_t i = 0; i < size; i++) {
+    uni.AddElem(*(Elems()+i));
+  }
+  //adding the second set
+  uni.Print();
+  for (size_t i = 0; i < a.Card(); i++) {
+    uni.AddElem(*(a.Elems()+i));
+  }
+  uni.Print();
+  int* intersection = this->Intersect(a);
+
+  //works until here
+  // for (size_t i = 0; i < size_t size = sizeof(intersection)/sizeof(*intersection); i++) {
+  //   uni.RmvElem(*(intersection+i));
+  // }
+  uni.Print();
+  return uni.Elems();
 }
-
-
-
 
 int main() {
 
   //init set
-  Set asdf;
+  Set asdf, fdsa;
   //add elements to asdf
   asdf.AddElem(3);
   asdf.AddElem(1);
@@ -123,8 +147,6 @@ int main() {
   //test Member() function
   asdf.Member(3);
 
-  //init set fdsa
-  Set fdsa;
   //add elements
   fdsa.AddElem(9);
   fdsa.AddElem(8);
@@ -142,6 +164,8 @@ int main() {
   //test Copy() function
   asdf.Copy(fdsa);
   asdf.Print();
+
+  //equality returns true
   std::cout << fdsa.Equal(asdf) << '\n';
 
   //test ElemIndex() function
@@ -153,8 +177,34 @@ int main() {
   asdf.Print();
 
 
+
+  //intersection plot
+  //the print doesn't know the size of the intersection array.
   std::cout << "intersection:" << '\n';
-  for (size_t i = 0; i < asdf.Card(); i++) {
-    std::cout << asdf.Intersect(fdsa)[i] << '\n';
+  std::cout << "{ ";
+  for (size_t i = 0; i < asdf.Card()-1; i++) {
+    std::cout << asdf.Intersect(fdsa)[i] << ", ";
   }
+  std::cout << " }" << '\n';
+
+
+  //union plot
+  //since cannot tell the size of the intersection array it cannot print it
+  //The problem is that I didn't use the Set class to declare the intersection set
+  //and the Union set.
+  std::cout << "union:" << '\n';
+  std::cout << "{ ";
+  int* uni = asdf.Union(fdsa);
+  for (size_t i = 0; i < asdf.Card() + fdsa.Card() - 1; i++) {
+    std::cout << *(uni+i) << ", ";
+  }
+  std::cout << " }" << '\n';
+
+  asdf = fdsa;
+
+  asdf >> 13;
+
+  asdf.Print();
+
+  return 0;
 }
--- set.h
+++ set.h
@@ -4,20 +4,75 @@ private:
   int size;
   int *set;
 public:
+  //getters
   int ElemIndex(int e);
   int MaxCard() {return maxCardinality;}; // The maximal cardinality of the set
   int* Elems() {return set;}; // An integer array containing all the elements of the set
   int Card() const {return size;}; // The effective number of elements in the set
+
+  //copy Constructor
+  Set(const Set &obj);
+
+  //Assignment Operator
+  Set& operator=(Set other) {
+      Copy(other);
+  }
+    // verify if a given integer is a member of the set
+  Set& operator&(int other){
+      Member(other);
+  }
+    // verify the equality of two sets
+  Set& operator==(Set other){
+      Equal(other);
+  }
+    // verify the inequality of two sets
+  Set& operator!=(Set other){
+      !Equal(other);
+  }
+    // compute the intersection of two sets
+  Set& operator*(Set other){
+      Intersect(other);
+  }
+    // compute the union of two sets
+  Set& operator+(Set other){
+      Union(other);
+  }
+  //these would be easy if i had the intersection and union figured out
+    // compute the difference of two sets
+  Set& operator-(Set other){
+
+  }
+    // verify the inclusion of one set inside another
+  Set& operator<=(Set other) {
+
+  }
+    // display the content of a set
+  Set& operator<<(int other){
+      Print();
+  }
+    // input the content of a set
+  Set& operator>>(int other){
+      this->AddElem(other);
+  }
+
   Set(){maxCardinality = 200; set = new int[maxCardinality];}; //default constructor
+  Set(int *arr){maxCardinality = 200; set = new int[maxCardinality]; set = arr;}; //default constructor
   Set(const int num) {maxCardinality = num; set = new int[maxCardinality];}; //One constructor with one argument: the maximal cardinality of the set
+
+  //setters
   void AddElem(const int e); // Add a new element to the set
   void RmvElem(int e); //  remove one element from the list (by shifting the other elements)
   bool Member(int e); // Verify if a given element is member of the set
+
   void Copy(Set& copyFrom); // copy one set in another
   bool Equal(Set& a); //verify the equality of two sets
+
   int* Intersect(Set& a); //compute the intersection of two sets
   int* Union(Set& a); //compute the union of two sets
+
+  //display set
   void Print(); //display the set, use the mathematics notation
 
-  virtual ~Set () { delete [] set;}; //One destructor
+  //destructor
+  virtual ~Set () { delete [] set;};
 };
