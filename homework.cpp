85527de5e24c074f4fda25858b4c98663de09ba3
Adam Nagy <an2208@nyu.edu>
2018-03-05 20:15:01 +0400

    added copy function

--- homework.cpp
+++ homework.cpp
@@ -1,22 +1,97 @@
 #include <iostream>
+#include "set.h"
 
-class Set {
-private:
-  int maxCardinality;
+bool Set::Member(int e) {
+  for (size_t i = 0; i < size; i++) {
+    if (e == set[i]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
 
-public:
-  int MaxCard(); // The maximal cardinality of the set
-Elems(); // An integer array containing all the elements of the set
-Card(); // The effective number of element in the set
-Set(); //default constructor
-Set (maxCardinality); //One constructor with one argument: the maximal cardinality of the set
-virtual ~Set (); //One destructor
-AddElem(e); // Add a new element to the set
-void RmvElem(); remove one element from the list (by shifting the other elements)
-Member(e); // Verify if a given element is member of the set
-Copy(); copy one set in another
-Equal(); verify the equality of two sets
-Intersect(); compute the intersection of two sets
-Union(); compute the union of two sets
-Print(); display the set, use the mathematics notation
-};
+void Set::Add(const int e){
+  int i = 0;
+  while (e < set[i]) {
+    i++;
+  }
+  set[i] = e;
+  size++;
+
+}
+
+int Set::ElemIndex(const int e) {
+  if (Member(e)) {
+    for (size_t i = 0; i < size; i++) {
+      if (e == set[i]) {
+        return i;
+      }
+    }
+  } else {
+    std::cout << "Element is not in the set." << '\n';
+  }
+}
+
+void Set::RmvElem(const int e) {
+  for (size_t i = ElemIndex(e); i < size; i++) {
+    set[i] = set[i+1];
+  }
+  size--;
+}
+
+void Set::Print() {
+  std::cout << "{ ";
+  for (size_t i = 0; i < size; i++) {
+    std::cout << set[i] << ", ";
+  }
+  std::cout << " }" << '\n';
+}
+
+void Set::Copy(Set& copyFrom) { //copy a to b
+  size = copyFrom.Card();
+  maxCardinality = copyFrom.MaxCard();
+  for (size_t i = 0; i < copyFrom.Card(); i++) {
+    set[i] = *(copyFrom.Elems()+i);
+  }
+}
+
+bool Set::Equal(Set& a, Set& b) {
+
+}
+
+int* Set::Intersect(Set& a, Set& b) {
+
+}
+
+int* Set::Union(Set& a, Set& b) {
+
+}
+
+
+
+
+int main() {
+  Set asdf;
+  asdf.AddElem(3);
+  asdf.AddElem(1);
+  asdf.AddElem(4);
+  Set fdsa;
+  fdsa.AddElem(9);
+  fdsa.AddElem(8);
+  fdsa.AddElem(7);
+  asdf.Print();
+  fdsa.Print();
+
+//test copyFrom
+
+  std::cout << asdf.Elems() << '\n';
+
+  asdf.Copy(fdsa);
+
+  asdf.Print();
+
+
+  std::cout << "maxcard " << asdf.MaxCard() << '\n';
+  std::cout << "card " << asdf.Card() << '\n';
+}
